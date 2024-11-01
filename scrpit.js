a79d4fa8924ecc0dba6e0fd8fb4049b8fcb480c3
XinnyDonut <xinxin.wang0703@gmail.com>
2024-11-01 18:19:41 +1300

    Add book and show in html

--- scrpit.js
+++ scrpit.js
@@ -1,26 +1,67 @@
 const library=[];
+const addBtn=document.querySelector(".addBtn");
+const modal=document.querySelector(".modal");
+const exitBtn=document.querySelector(".close");
+const submitBtn=document.querySelector("#submitBtn");
+const bookContainer=document.querySelector('#bookContainer');
+let bookId=0;
 
 
-function Book(title,author,year){
+
+function Book(title,author,page){
     this.title=title;
     this.author=author;
-    this.year=year;
+    this.page=page;
+    this.read=false;
+    this.setRead=function(){
+        this.read=true;
+    }
 
 }
 function addBookToLib(lib,book){
     lib.push(book);
 }
+function display(book){
+    
+        console.log(book);
+        const ID=bookId++;
+        const bookDiv=document.createElement("div");
+        const title=document.createElement("h2");
+        const author=document.createElement("h3");
+        const pages=document.createElement("h3");
+        const deleteBtn=document.createElement("button");
 
-function display(){
-    library.forEach(book=>console.log(book));
+        bookDiv.setAttribute("id",`book-${ID}`);
+        bookDiv.classList.add("book-div");
+
+        title.textContent=book.title;
+        author.textContent=`author: ${book.author}`;
+        pages.textContent=`pages: ${book.pages}`;
+        deleteBtn.textContent="Delete";
+        deleteBtn.onclick=function(){         
+            const i=library.indexOf(book);
+            library.splice(i,1);
+            bookDiv.remove();        
+        }
+        bookDiv.appendChild(title);
+        bookDiv.appendChild(author);
+        bookDiv.appendChild(pages);
+        bookDiv.appendChild(deleteBtn);
+        bookContainer.appendChild(bookDiv);   
 }
 
-
-
-
-
-const b1 =new Book("hello","Jk",1988);
-const b2 =new Book("harry","harvard",1908);
-addBookToLib(library,b1);
-addBookToLib(library,b2);
-display();
+addBtn.addEventListener('click',()=>modal.style.display="block");
+exitBtn.addEventListener('click',()=>modal.style.display="none");
+submitBtn.onclick=function(){
+    const title=document.querySelector("#title").value;
+    const author=document.querySelector("#author").value;
+    const pages=parseInt(document.querySelector("#pages").value);
+    if(title&&author&&pages&&!isNaN(pages)){
+        const b=new Book(title,author,pages);
+        addBookToLib(library,b);
+        modal.style.display="none";
+        display(b);
+    }else{
+        alert("You need to fill in all fields")
+    }   
+}
